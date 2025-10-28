af462a8000b8ee5327260c67b8b9730e6fb655c4
jorscastropragma <jors.castro@pragma.com.co>
2025-10-28 16:15:19 -0500

    sonar

--- src/test/java/com/plazoleta/plazoleta/domain/usecase/RestauranteUseCaseTest.java
+++ src/test/java/com/plazoleta/plazoleta/domain/usecase/RestauranteUseCaseTest.java
@@ -14,8 +14,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
